2492191c96f4ea210d93b65a23685b1d2a353a70
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2017-07-02 16:10:19 -0700

    Initial project structure setup.

---
+++ AppliedDA-book-programs/src/main/scala/com/equalinformation/book/appliedDA/scala/Test3.scala
@@ -0,0 +1,8 @@
+package com.equalinformation.book.appliedDA.scala
+
+/**
+  * Created by bpupadhyaya on 7/2/17.
+  */
+object Test3 {
+
+}
---
+++ AppliedDA-book-programs/src/test/java/com/equalinformation/book/appliedDA/java/test/Test2.java
@@ -0,0 +1,7 @@
+package com.equalinformation.book.appliedDA.java.test;
+
+/**
+ * Created by bpupadhyaya on 7/2/17.
+ */
+public class Test2 {
+}
