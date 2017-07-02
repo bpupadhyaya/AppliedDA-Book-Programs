da0df1c14ea4243a1fc3234c24bc2c0c7225a52c
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2017-07-02 16:37:44 -0700

    Commit chain test.

--- AppliedDA-book-programs/src/main/scala/com/equalinformation/book/appliedDA/scala/Test3.scala
+++ AppliedDA-book-programs/src/main/scala/com/equalinformation/book/appliedDA/scala/Test3.scala
@@ -4,5 +4,7 @@ package com.equalinformation.book.appliedDA.scala
   * Created by bpupadhyaya on 7/2/17.
   */
 object Test3 {
-
+  def main(args: Array[String]): Unit = {
+    println("commit chain test")
+  }
 }
